ac1f0ef250b3d83668826711f300128b0baef7ff
viviankamo <vivian.kamo@elev.ga.ntig.se>
2021-05-19 22:56:02 +0200

    fullstack

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+/* varje gång man skrollar ner till statistik så ska siffrorna börja visa sig och räkna upp till det antal som är skrivet. 
+Detta gör det möjligt, bestämmer hur lång tid det ska ta så att alla siffrorna stannar samtidigt och vid en position 
+(då man kan se siffrorna) de ska börja öka. */
 let nCount = selector => {
     $(selector).each(function () {
       $(this)
@@ -24,7 +28,7 @@ let nCount = selector => {
     }
   });
 
-  
+  /* Fixar så att navigationen följer med hela vägen ner efter man inte ser home-page längre */
   let navbar = $(".navbar");
   
   $(window).scroll(function () {
